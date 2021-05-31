3ae37069f72c8644a5f683c6757bf13b8c87235d
Anushek Prasal <anushekprasal@gmail.com>
2021-05-31 16:44:52 +0530

    base: Introduce automated Sleep Mode [1/2]
    [cjh1249131356]:
    - Adapt to S
    - Dropped AOD toggle in favor of scheduled AOD feature
    - Dropped Aggressive battery toggle
    
    [joeyhuab]
    * Adapt to A13 QPR2 after b8d6c0a.
    * Drop Location toggle for A14 QPR2.
    
    Squashed:
    
    	From: cjh1249131356 <cjh1249131356@gmail.com>
    	Date: Sat, 16 Apr 2022 11:41:31 +0800
    	Subject: [PATCH] base: Always turn on sensors when disabling sleep mode
    
    	* Also verify once to make sure sensors are turned on.
    
    	From: minaripenguin <minaripenguin@users.noreply.github.com>
    	Date: Mon, 25 Nov 2024 09:37:43 +0800
    	Subject: [PATCH] SleepModeController: Fix handler usages
    
    	From: minaripenguin <minaripenguin@users.noreply.github.com>
    	Date: Mon, 25 Nov 2024 11:14:01 +0800
    	Subject: [PATCH] services: Fix sleep mode service initialization
    
    Change-Id: I85fc36e6db7ceeca1a1031f71f3882a776864814
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
+++ packages/SystemUI/src/com/android/systemui/lineage/LineageModule.kt
@@ -23,6 +23,7 @@ import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.PowerShareTile
+import com.android.systemui.qs.tiles.SleepModeTile
 import com.android.systemui.qs.tiles.ProfilesTile
 import com.android.systemui.qs.tiles.ReadingModeTile
 import com.android.systemui.qs.tiles.SyncTile
@@ -72,6 +73,12 @@ interface LineageModule {
     @IntoMap
     @StringKey(PowerShareTile.TILE_SPEC)
     fun bindPowerShareTile(powerShareTile: PowerShareTile): QSTileImpl<*>
+
+     /** Inject SleepModeTile into tileMap in QSModule */
+     @Binds
+     @IntoMap
+     @StringKey(SleepModeTile.TILE_SPEC)
+     fun bindSleepModeTile(sleepModeTile: SleepModeTile): QSTileImpl<*>
 
     /** Inject ProfilesTile into tileMap in QSModule */
     @Binds
